0f86e2f87d8b2bbf9f6370d050212b88d7a77025
jollen <jollen@moko365.com>
2015-09-15 16:03:00 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Rephone.co Website
+
+The rephone.co website is open source.
+
+1. To discuss general issues, please [Open an issue](https://github.com/rephone/discussions/issues/new)
+2. To anwser a question, please [Answer a question](https://github.com/rephone/discussions/issues)
+3. To add a local rephone user group, please Open an issue](https://github.com/rephone/rephone.co/issues) or send a pull request
+4. Any CRUD of rephone.co, please send a pull request
---
+++ app.js
@@ -0,0 +1,48 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// config for rephone.co
+app.disable('x-powered-by');
+app.set('strict routing', true);
+app.set('project-name', 'Rephone Community');
+app.set('project-title', 'Rephone Community');
+app.set('company-name', 'Seeed Studio');
+app.set('cdn-server', '');
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// global locals
+app.locals.projectName = app.get('project-name');
+app.locals.projectTitle = app.get('project-title');
+app.locals.cdnServer = app.get('cdn-server');
+app.locals.cacheBreaker = '?20150915';
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
